3c904a9ddd5b09df1bed115bcf59dc7983dc3150
erik <erik_richard@outlook.com>
2025-07-25 16:49:41 -0300

    adding vehicle simulator

--- app/src/main/java/com/senai/myapplication/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/senai/myapplication/ui/home/HomeViewModel.kt
@@ -3,6 +3,9 @@ package com.senai.myapplication.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collect
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +13,23 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    private val vehicleSensorSimulator = VehicleSensorSimulator("Velocidade")
+    private val canBusSimulator = VehicleCanBusSimulator()
+
+    init {
+        viewModelScope.launch {
+            canBusSimulator.canMessageFlow.collect { message ->
+                _text.postValue("Velocidade: CAN Message Received: ID=${message.id}, Data=${message.data.joinToString()}")
+            }
+        }
+        sendRandomSensorData()
+    }
+
+    fun sendRandomSensorData() {
+        val randomValue = vehicleSensorSimulator.readSensorData()
+        val message = CanMessage(0x123, byteArrayOf(randomValue.toByte()))
+        canBusSimulator.sendMessage(message)
+    }
+
 }
